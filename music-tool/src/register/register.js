ba7a4553e43238e2618ea45b23070b9b3105f648
david <dvlasic000@gmail.com>
2024-10-31 18:35:48 +0000

    Create required elements for registration form

--- music-tool/src/register/register.js
+++ music-tool/src/register/register.js
@@ -12,8 +12,53 @@ const Register = () => {
     const handleRegister = () => {
         console.log("Registering with:", { username, email, password });
     };
+    return (
+        <Box className="login-container">
+            <Paper className="login-paper">
+                <Typography variant="h5" className="login-title">
+                    Register
+                </Typography>
 
-    
+                <TextField
+                    label="Username"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <TextField
+                    label="Email"
+                    type="email"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <TextField
+                    label="Password"
+                    type="password"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <Button
+                    variant="contained"
+                    color="primary"
+                    fullWidth
+                    onClick={handleRegister}
+                    className="login-button"
+                >
+                    Register
+                </Button>
+            </Paper>
+        </Box>
+    );
 };
 
 export default Register;
+
