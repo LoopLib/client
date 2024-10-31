a1ab320973a4153e7c0530cfd3a1ba8d19860f89
david <dvlasic000@gmail.com>
2024-10-31 12:55:47 +0000

    Create FileUpload components

---
+++ music-tool/src/FileUpload.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { Box, Button, Typography, Paper } from "@mui/material";
+import UploadFileIcon from "@mui/icons-material/UploadFile";
+import "./FileUpload.css"; // Import the CSS file
+
+const FileUpload = () => {
+ 
+
+  return (
+    <Box className="file-upload-container">
+     
+    </Box>
+  );
+};
+
+export default FileUpload;
