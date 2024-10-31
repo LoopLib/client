e627a370ec5a3ad7917eaeb62ec6f08b1bbce6ae
david <dvlasic000@gmail.com>
2024-10-31 18:30:16 +0000

    Create login form with required elements

--- music-tool/src/login/login.js
+++ music-tool/src/login/login.js
@@ -3,15 +3,53 @@ import { Box, Button, Typography, Paper, TextField } from "@mui/material";
 import "./login.css";
 
 const Login = () => {
+    // State to store the username and password
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
+    // Function to handle the login action
     const handleLogin = () => {
         console.log("Logging in with:", { username, password });
-        
-    };
 
-   
+    };
+    // Return the login form
+    return (
+        <Box className="login-container">
+            <Paper className="login-paper">
+                <Typography variant="h5" className="login-title">
+                    Login
+                </Typography>
+
+                <TextField
+                    label="Username"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <TextField
+                    label="Password"
+                    type="password"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <Button
+                    variant="contained"
+                    color="primary"
+                    fullWidth
+                    onClick={handleLogin}
+                    className="login-button"
+                >
+                    Login
+                </Button>
+            </Paper>
+        </Box>
+    );
 };
 
 export default Login;
