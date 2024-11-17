60395fc043e8a02c289e6cc1b416f72bffad22fc
david <dvlasic000@gmail.com>
2024-11-17 18:34:50 +0000

    Change the color of the background and some elements

--- music-tool/src/login/login.js
+++ music-tool/src/login/login.js
@@ -26,38 +26,36 @@ const Login = () => {
     // Return the login form
     return (
         <Box className="login-container">
-            <Paper className="login-paper">
-                <Typography variant="h5" className="login-title">
-                    LOGIN
-                </Typography>
+            <Typography variant="h5" className="login-title">
+                LOGIN
+            </Typography>
 
-                <TextField
-                    label="Username"
-                    variant="outlined"
-                    fullWidth
-                    margin="normal"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-                <TextField
-                    label="Password"
-                    type="password"
-                    variant="outlined"
-                    fullWidth
-                    margin="normal"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <Button
-                    variant="contained"
-                    color="primary"
-                    fullWidth
-                    onClick={handleLogin}
-                    className="login-button"
-                >
-                    Login
-                </Button>
-            </Paper>
+            <TextField
+                label="Username"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <TextField
+                label="Password"
+                type="password"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+                variant="contained"
+                color="primary"
+                fullWidth
+                onClick={handleLogin}
+                className="login-button"
+            >
+                Login
+            </Button>
         </Box>
     );
 };
