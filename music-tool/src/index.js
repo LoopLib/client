6795146c044723703dcd7317ce64d1d04cf6e37d
david <dvlasic000@gmail.com>
2024-11-17 14:15:05 +0000

    Add additional imports

--- music-tool/src/index.js
+++ music-tool/src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <BrowserRouter>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
