a661d3816aedaf628cd9a151177fed9f92a432d8
david <dvlasic000@gmail.com>
2024-11-17 14:11:32 +0000

    Update App.js to display navbar

--- music-tool/src/App.js
+++ music-tool/src/App.js
@@ -1,20 +1,22 @@
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import FileUpload from "./upload/upload";
 import HomePage from "./homepage/homepage";
 import Login from "./login/login";
 import Register from "./register/register";
+import Layout from "./layout/layout";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/homepage" element={<HomePage />} />
-        <Route path="/upload" element={<FileUpload />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/homepage" element={<HomePage />} />
+          <Route path="/upload" element={<FileUpload />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 }
