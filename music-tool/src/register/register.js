4c8e86464d51d382a3741b909c5e677839ad657b
david <dvlasic000@gmail.com>
2024-11-17 18:36:01 +0000

    Remove paper element from registration page

--- music-tool/src/register/register.js
+++ music-tool/src/register/register.js
@@ -26,48 +26,46 @@ const Register = () => {
     };
     return (
         <Box className="login-container">
-            <Paper className="login-paper">
-                <Typography variant="h5" className="login-title">
-                    REGISTER
-                </Typography>
+            <Typography variant="h5" className="login-title">
+                REGISTER
+            </Typography>
 
-                <TextField
-                    label="Username"
-                    variant="outlined"
-                    fullWidth
-                    margin="normal"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-                <TextField
-                    label="Email"
-                    type="email"
-                    variant="outlined"
-                    fullWidth
-                    margin="normal"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <TextField
-                    label="Password"
-                    type="password"
-                    variant="outlined"
-                    fullWidth
-                    margin="normal"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
+            <TextField
+                label="Username"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <TextField
+                label="Email"
+                type="email"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+                label="Password"
+                type="password"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
 
-                <Button
-                    variant="contained"
-                    color="primary"
-                    fullWidth
-                    onClick={handleRegister}
-                    className="login-button"
-                >
-                    Register
-                </Button>
-            </Paper>
+            <Button
+                variant="contained"
+                color="primary"
+                fullWidth
+                onClick={handleRegister}
+                className="login-button"
+            >
+                Register
+            </Button>
         </Box>
     );
 };
