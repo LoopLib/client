cdf9059362f71d42e83b39dba311da3a4a992638
david <dvlasic000@gmail.com>
2024-11-17 14:23:54 +0000

    Update navbar

--- music-tool/src/navbar/navbar.js
+++ music-tool/src/navbar/navbar.js
@@ -1,22 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./navbar.css"; // Create a CSS file for styling
+import "./navbar.css";
 
 function Navbar() {
   return (
     <nav className="navbar">
+      <div className="navbar-brand">
+        <Link to="/" className="navbar-logo">BrandName</Link>
+      </div>
       <ul className="navbar-list">
         <li className="navbar-item">
-          <Link to="/homepage">Home</Link>
+          <Link to="/homepage" className="navbar-link">Home</Link>
         </li>
         <li className="navbar-item">
-          <Link to="/upload">Upload</Link>
+          <Link to="/upload" className="navbar-link">Upload</Link>
         </li>
         <li className="navbar-item">
-          <Link to="/">Login</Link>
+          <Link to="/" className="navbar-link">Login</Link>
         </li>
         <li className="navbar-item">
-          <Link to="/register">Register</Link>
+          <Link to="/register" className="navbar-link">Register</Link>
         </li>
       </ul>
     </nav>
